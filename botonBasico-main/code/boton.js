2305d66fde5d4df5a82de33d84afbe5f27e5a4a8
larariancho <lrianchob01@educantabria.es>
2026-01-20 13:42:14 +0100

    Add files via upload

---
+++ botonBasico-main/code/boton.js
@@ -0,0 +1,10 @@
+const hazAlgo = document.getElementById("elBoton");
+ 
+hazAlgo.onclick = function () {
+ document.body.style.backgroundColor = "rgb(173, 140, 102)";
+}
+const hazOtraCosa = document.getElementById("otroBoton");
+ 
+hazOtraCosa.onclick = function () {
+ document.body.style.backgroundColor = "rgb(89, 102, 121)";
+}
